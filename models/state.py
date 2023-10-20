5de8ea4ab6225073085aea9e98292b4f7d5b0305
Blackhat-red-team <ibrahimsaid909000@gmail.com>
2023-10-20 15:16:43 +0300

    fix  state moduels.py

--- models/state.py
+++ models/state.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-
+from models import storage
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 from os import getenv
-
+from models.city import City  # Import the City class
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade='delete', backref='state')
+
     if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
-            city_list= []
-            for city in models.storage.all(city).values():
+            city_list = []
+            for city in storage.all(City).values():
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
