41232153e60d6c942287097f21c9b7f0b813efae
Blackhat-red-team <ibrahimsaid909000@gmail.com>
2023-10-22 12:55:38 +0300

    fix

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -7,6 +7,7 @@ Routes:
 from models import storage
 from flask import Flask
 from flask import render_template
+from models.state import State
 
 app = Flask(__name__)
 
@@ -14,7 +15,7 @@ app = Flask(__name__)
 @app.route("/hbnb_filters", strict_slashes=False)
 def hbnb_filters():
     """Display is filters the main HBnB  HTML page."""
-    states = storage.all("State")
+    states = storage.all(State)
     amenities = storage.all("Amenity")
     return render_template("10-hbnb_filters.html",
                            states=states, amenities=amenities)
